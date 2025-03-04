b2a74f15ea7a99bf0fee13543641ec580d076ed8
Yann Hoffmann <yann.hoffmaaann@gmail.com>
2025-03-04 16:22:52 +0100

    Improve table generator

--- yann_utils/phd.py
+++ yann_utils/phd.py
@@ -1,32 +1,55 @@
-# %%
 import pandas as pd
 import subprocess
 import os
+import tempfile
+import shutil
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
 
-def csv_to_latex_png(df, filename, caption, index):
-  # 1. Convert the df to LaTeX table format
-  latex_code = r"""\documentclass{standalone}
+def csv_to_latex_png(df, caption="Table", index=False):
+  with tempfile.TemporaryDirectory() as temp_dir:
+    latex_file = os.path.join(temp_dir, "table.tex")
+    pdf_file = os.path.join(temp_dir, "table.pdf")
+    png_file = os.path.join(temp_dir, "table.png")
+    ppm_file = os.path.join(temp_dir, "table-1.png")
+
+    # LaTeX document
+    latex_code = r"""\documentclass{standalone}
 \usepackage{booktabs}
 \usepackage{preview}
 \PreviewEnvironment{tabular}
 \begin{document}
 """
-  latex_code += df.to_latex(index=index, escape=True, caption=caption)
-  latex_code += r"\end{document}"
+    latex_code += df.to_latex(index=index, escape=True, caption=caption)
+    latex_code += r"\end{document}"
 
-  with open("out/temp_table.tex", "w") as latex_file:
-    latex_file.write(latex_code)
+    # Write LaTeX file
+    with open(latex_file, "w") as f:
+      f.write(latex_code)
 
-  subprocess.call(
-    ["pdflatex", "-interaction=nonstopmode", "-output-directory=out", "temp_table.tex"]
-  )
-  subprocess.call(["pdftoppm", "-png", f"out/temp_table.pdf", "temp_table"])
-  os.rename("temp_table-1.png", filename)
+    # Generate PDF
+    subprocess.call(
+      [
+        "pdflatex",
+        "-interaction=nonstopmode",
+        "-output-directory",
+        temp_dir,
+        latex_file,
+      ],
+      stdout=subprocess.DEVNULL,
+      stderr=subprocess.DEVNULL,
+    )
 
+    # Convert PDF to PNG
+    subprocess.call(["pdftoppm", "-png", pdf_file, os.path.join(temp_dir, "table")])
 
-#%%
-df = pd.read_clipboard()
-csv_to_latex_png(
-  df, filename="noise1.png", caption="Reset algorithm", index=False
-)
+    # Rename generated file
+    shutil.move(ppm_file, png_file)
+
+    # Display image inline
+    img = mpimg.imread(png_file)
+    plt.figure(figsize=(10, 4))
+    plt.axis("off")
+    plt.imshow(img)
+    plt.show()
