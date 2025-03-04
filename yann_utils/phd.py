24fe6160b96eb58af712f550c14237392c0185ed
Yann Hoffmann <yann.hoffmaaann@gmail.com>
2025-03-04 16:17:03 +0100

    Improve table generator

--- yann_utils/phd.py
+++ yann_utils/phd.py
@@ -1,10 +1,10 @@
-#%%
+# %%
 import pandas as pd
 import subprocess
 import os
 
 
-def csv_to_latex_png(df, png_filename, caption):
+def csv_to_latex_png(df, filename, caption, index):
   # 1. Convert the df to LaTeX table format
   latex_code = r"""\documentclass{standalone}
 \usepackage{booktabs}
@@ -12,20 +12,21 @@ def csv_to_latex_png(df, png_filename, caption):
 \PreviewEnvironment{tabular}
 \begin{document}
 """
-  latex_code += df.to_latex(index=True, escape=True, caption=caption)
+  latex_code += df.to_latex(index=index, escape=True, caption=caption)
   latex_code += r"\end{document}"
 
   with open("out/temp_table.tex", "w") as latex_file:
     latex_file.write(latex_code)
 
-  subprocess.call(["pdflatex", "-interaction=nonstopmode", "-output-directory=out", "temp_table.tex"])
-  subprocess.call(["pdftoppm", "-png", "out/temp_table.pdf", "temp_table"])
-  os.rename("temp_table-1.png", "latex_table.png")
+  subprocess.call(
+    ["pdflatex", "-interaction=nonstopmode", "-output-directory=out", "temp_table.tex"]
+  )
+  subprocess.call(["pdftoppm", "-png", f"out/temp_table.pdf", "temp_table"])
+  os.rename("temp_table-1.png", filename)
 
 
 #%%
 df = pd.read_clipboard()
-csv_to_latex_png(df, "latex_table.png", "Reset algorithm")
-
-#%%
-df
+csv_to_latex_png(
+  df, filename="noise1.png", caption="Reset algorithm", index=False
+)
